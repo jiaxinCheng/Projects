3ce594adbe815b2248308fd32589b321f4b8d7b9
Jiaxin Cheng <jiaxinch@usc.edu>
2016-02-16 20:20:31 -0800

    hw

--- hw3/test.cpp
+++ hw3/test.cpp
@@ -38,20 +38,37 @@ protected:
 	// Objects declared here can be used by all tests in the test case.
 	LListInt list;
 	LListInt list2;
+	LListInt list3;
 	
 };
 
 TEST_F(LListIntTest, Assign) {
-	// Exepct 0th element to be 1, 1st elemnt to be 2, etc.
+	// assign something to itself
+	list2 = list2;
+	for(int i = 0;i < 4;++i){
+		EXPECT_EQ(1,list2.get(i));
+	}
+	EXPECT_EQ(4,list2.size());
+	//normally assign something
 	list2 = list;
 	for (int i = 0;i<3;++i){
 		EXPECT_EQ(list.get(i),list2.get(i));
 	}
 	EXPECT_EQ(3,list2.size());
+	//assign something to a empty list 
+	list3 = list;
+	for (int i = 0;i<3;++i){
+		EXPECT_EQ(list.get(i),list3.get(i));
+	}
+	EXPECT_EQ(3,list3.size());
+	list3.clear();
+	list = list3;//assign something empty to the list
+	EXPECT_EQ(0,list.size());
+	
 }
 
 TEST_F(LListIntTest, Plus) {
-	LListInt list3 = list + list2;
+	list3 = list + list2;//normally add
 	for (int i=0;i <3;i++){
 		EXPECT_EQ(list.get(i),list3.get(i));
 	}
@@ -59,6 +76,11 @@ TEST_F(LListIntTest, Plus) {
 		EXPECT_EQ(1,list3.get(j));
 	}
 	EXPECT_EQ(7,list3.size());
+	list.clear();
+	list3 = list + list2;//add empty list 
+	for (int j =1;j<4;++j){
+		EXPECT_EQ(1,list3.get(j));
+	}
 }
 TEST_F(LListIntTest, Bracket) {
 	EXPECT_EQ(list.get(0),list[0]);
