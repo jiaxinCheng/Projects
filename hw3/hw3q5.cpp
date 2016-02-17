53c18e46530eed68e742924e45a79c4a9248b6d7
Jiaxin Cheng <jiaxinch@usc.edu>
2016-02-16 19:46:25 -0800

    hw

--- hw3/hw3q5.cpp
+++ hw3/hw3q5.cpp
@@ -1,4 +1,4 @@
-#include "stackint.cpp"
+#include "stackint.h"
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -17,22 +17,22 @@ int compute(string src){
 	char a;
 	while(ss >> a){
 		if (a == '<'){
-			if(!stack.empty()){
-				if (stack.top() > 0 ){
+			if(!stack.empty()){// will check wether the stack is empty after every pop
+				if (stack.top() > 0 ){// it is impossible for a '<' to have a number in front
 					return -1;
 				}
 				else if (stack.top() == CLOS_PAREN){
 					return -1;
 				}
 				else {
-					stack.push(LEFT_SHIFT);
+					stack.push(LEFT_SHIFT); // if this is the correct place, then push the shiftleft
 				}
 			}
 			else {
-				stack.push(LEFT_SHIFT);				
+				stack.push(LEFT_SHIFT);	// if stack is empty, we can push the shift right		
 			}
 		}
-		else if (a == '>'){
+		else if (a == '>'){// the following code is basically the same as the block above
 			if(!stack.empty()){
 				if (stack.top() > 0 ){
 					return -1;
@@ -49,20 +49,20 @@ int compute(string src){
 			}
 		}
 		else if (a == '+' ){
-			if (stack.empty()){
+			if (stack.empty()){// it is impossible to have a '+' in the front
 				return -1;
 			}
 			else if (stack.top() == LEFT_SHIFT || stack.top() == RIGH_SHIFT || stack.top() == '*'){
 				return -1;
 			}
-			else if (stack.top() == OPEN_PAREN  || stack.top() == '+'){
+			else if (stack.top() == OPEN_PAREN  || stack.top() == '+'){// rule out all the impossible combinations 
 				return -1;
 			}
 			else {				
 				stack.push(PLUS);
 			} 
 		}
-		else if (a == '*'){
+		else if (a == '*'){// the following code is basically the same as the block above
 			if (stack.empty()){
 				return -1;
 			}
@@ -76,20 +76,20 @@ int compute(string src){
 				stack.push(MULTI);
 			} 
 		}
-		else if (a == '('){
+		else if (a == '('){// if there is a open paren, and there is a close before it, reject
 			if (!stack.empty()){
 				if (stack.top() == CLOS_PAREN){
 					return -1;
 				}
 				else{
-					stack.push(OPEN_PAREN);
+					stack.push(OPEN_PAREN);// else we can push it safely 
 				}
 			}
 			else {
 				stack.push(OPEN_PAREN);
 			}
 		}
-		else if (a == ')'){
+		else if (a == ')'){ //evaluate the formula in the stack
 			if(stack.empty()){
 				return -1;
 			}
@@ -109,7 +109,7 @@ int compute(string src){
 				if (stack.empty()){
 					return -1;
 				}				
-				if (stack.top() == MULTI){
+				if (stack.top() == MULTI){//if we see a multi
 					stack.pop();
 					if (stack.empty()){
 						return -1;
@@ -119,7 +119,7 @@ int compute(string src){
 					if (stack.empty()){
 						return -1;
 					}
-					while(stack.top() == RIGH_SHIFT || stack.top() == LEFT_SHIFT){
+					while(stack.top() == RIGH_SHIFT || stack.top() == LEFT_SHIFT){// while we have the shift left and right, continue times two or half
 						if (stack.top() == RIGH_SHIFT){
 							temp2 = temp2/2;
 							stack.pop();
@@ -134,16 +134,16 @@ int compute(string src){
 								return -1;
 							}
 						}
-						if (stack.empty()){
+						if (stack.empty()){//check empty
 							return -1;
 						}
 					}
-					if (stack.top() == PLUS){
+					if (stack.top() == PLUS){// inside two parenth, if we see a multi first and a plus, reject
 						return -1;
 					}
-					re = re * temp2;
+					re = re * temp2; //times two of the number together
 				}
-				if (stack.top() == PLUS){
+				if (stack.top() == PLUS){ // this block is the same as above 
 					stack.pop();
 					if (stack.empty()){
 						return -1;
@@ -177,14 +177,14 @@ int compute(string src){
 					}
 					re = re + temp2;
 				}
-				if (stack.top() == RIGH_SHIFT){
+				if (stack.top() == RIGH_SHIFT){ //in the parenth, if we see a right, then times half
 					re = re/2;
 					stack.pop();
 					if (stack.empty()){
 						return -1;
 					}
 				}
-				if (stack.top() == LEFT_SHIFT){
+				if (stack.top() == LEFT_SHIFT){// if we see a left, times two 
 					re = re*2;
 					stack.pop();
 					if (stack.empty()){
@@ -193,17 +193,17 @@ int compute(string src){
 				}
 			}
 			if (!stack.empty()){
-				if (stack.top() == OPEN_PAREN){
+				if (stack.top() == OPEN_PAREN){// pop the open parenth corresponding to the close 
 					stack.pop();
 				}
 			}
 			stack.push(re);
 		}
-		else if (a >= 48 && a <= 57){
+		else if (a >= 48 && a <= 57){// if there is a number, then we push as long there is no number before it
 			if (stack.empty()){
 				stack.push(a-48);
 			}
-			else if (stack.top() >0){
+			else if (stack.top() >0){ // if there is a number before the number, times the previous one by 10 and add to number we have
 				int topnum =stack.top();
 				topnum = 10 *topnum+((int)a-48);
 				stack.pop();
@@ -216,11 +216,11 @@ int compute(string src){
 	}	
 	int final =  stack.top();
 	stack.pop();
-	if (stack.empty() && final > 0){
+	if (stack.empty() && final > 0){// if there is nothing remaining, return 
 		return final;
 	}
 	else {
-		while(!stack.empty()){
+		while(!stack.empty()){// if there is somthing remaining, it must be some shifts, finish them and pop
 			if (stack.top() == RIGH_SHIFT){
 				final = final/2;
 				stack.pop();
@@ -229,7 +229,7 @@ int compute(string src){
 				final = final *2;
 				stack.pop();
 			}
-			else {
+			else {// if what remaining are not parenth, then it is malformed
 				return -1;
 			}
 		}
@@ -247,6 +247,7 @@ int main(int argc, char *argv[]){
 		getline(in, temp);
 		int result;
 		if(!temp.empty()){
+			cout << temp << endl;
 			result =compute(temp);
 			if (result == -1){
 				cout << "Malfunction" << endl;
